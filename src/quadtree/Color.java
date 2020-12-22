82f5ae64e430e43b80195c67353bc6ad7d81f2ae
oshevchuk27 <oshevchuk@brynmawr.edu>
2020-12-22 00:21:20 -0500

    Added color class

--- src/quadtree/Color.java
+++ src/quadtree/Color.java
@@ -1,6 +1,37 @@
 package quadtree;
-
+/**
+ * A class that stores pixels
+ * color components.
+ *
+ * @author Olga Shevchuk
+ *
+ */
 public class Color {
-	
+
+	private int red;
+	private int blue;
+	private int green;
+
+
+	public Color(int red, int blue, int green) {
+		this.red = red;
+		this.blue = blue;
+		this.green = green;
+	}
+
+	public int getRed() {
+		return this.red;
+	}
+
+	public int getBlue() {
+		return this.blue;
+	}
+
+	public int getGreen() {
+		return this.green;
+	}
+
+
+
 
 }
